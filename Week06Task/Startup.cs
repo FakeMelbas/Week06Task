90ed948099e15848f2a348c9c40f7ffc84be6ffb
Dylan Peterson <101408728@student.swin.edu.au>
2018-09-03 10:21:10 +1000

    Whole Task

---
+++ Week06Task/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week06Task.Startup))]
+namespace Week06Task
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
